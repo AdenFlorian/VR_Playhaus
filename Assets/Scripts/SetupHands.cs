9d87f30b2f1f8a7a4fca8089205acf144484c2ef
AdenFlorian <AdenFlorian@gmail.com>
2016-05-17 12:58:59 -0500

    made setup hands script and hands prefabs

---
+++ Assets/Scripts/SetupHands.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetupHands : MonoBehaviour {
+
+    public GameObject controllerLeftGO;
+    public GameObject controllerRightGO;
+
+    // Use this for initialization
+    void Start () {
+        // Get reference to controllers
+        GameObject handGOResource = Resources.Load<GameObject>("Hand");
+        GameObject handLeftGO = Instantiate<GameObject>(handGOResource);
+        GameObject handRightGO = Instantiate<GameObject>(handGOResource);
+        handLeftGO.transform.parent = controllerLeftGO.transform;
+        handRightGO.transform.parent = controllerRightGO.transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
