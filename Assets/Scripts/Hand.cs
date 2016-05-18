be7757e0bde9096f7658cd039aa49b05ec9c3f93
jeffski13 <jeffski@okstate.edu>
2016-05-17 23:45:44 -0500

    made a hand script

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hand : MonoBehaviour {
+
+    Transform parentController;
+
+    // Use this for initialization
+    void Start () {
+        parentController = transform.parent;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = parentController.position;
+	}
+}
