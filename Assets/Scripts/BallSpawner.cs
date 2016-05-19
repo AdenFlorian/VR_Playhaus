04054009724d7186d4e3d61982c45d8dcf3c1437
David Valachovic <AdenFlorian@gmail.com>
2016-05-18 21:04:43 -0500

    got rigidbody hands working really well, also ball spawner

---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallSpawner : MonoBehaviour {
+
+    public SteamVR_TrackedObject LeftControllerTrackedObj;
+    public SteamVR_TrackedObject RightControllerTrackedObj;
+    public GameObject ballPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SteamVR_Controller.Device deviceLeft = null;
+        SteamVR_Controller.Device deviceRight = null;
+        try {
+            deviceLeft = SteamVR_Controller.Input((int)LeftControllerTrackedObj.index);
+            deviceRight = SteamVR_Controller.Input((int)RightControllerTrackedObj.index);
+        } catch (System.IndexOutOfRangeException) {
+            // Not sure what to do with this yet, only happens on first update
+            Debug.Log("IndexOutOfRangeException");
+            return;
+        }
+
+
+        //if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger)) {
+        //    Debug.Log("Holding 'Touch' on Trigger");
+        //}
+
+        if (deviceLeft.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger)) {
+            Debug.Log("Did 'TouchDown' on left Trigger");
+            SpawnBall();
+        }
+        if (deviceRight.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger)) {
+            Debug.Log("Did 'TouchDown' on right Trigger");
+            SpawnBall();
+        }
+    }
+
+    void SpawnBall() {
+        GameObject newBallGO = Instantiate<GameObject>(ballPrefab);
+        newBallGO.transform.position = transform.position;
+    }
+}
--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -8,23 +8,70 @@ public class Hand : MonoBehaviour {
     new Rigidbody rigidbody;
     
     void Start () {
-        //parentController = transform.parent;
         rigidbody = GetComponent<Rigidbody>();
 	}
 	
 	void FixedUpdate () {
-        Vector3 toParentPos = parentController.position - transform.position;
-
-        toParentPos = toParentPos / Time.fixedDeltaTime;
-
-        rigidbody.velocity = toParentPos;
-
-        Vector3 toParentRot = Quaternion.RotateTowards(transform.rotation, parentController.rotation, 0f).eulerAngles;
-
-        rigidbody.angularVelocity = toParentRot;
+        DoPosition();
+        DoRotation();
     }
 
     public void SetTarget(Transform target) {
         parentController = target;
+    }
+
+    private void DoPosition() {
+        // Get vector from current position to parent's position
+        Vector3 forceToParentPosition = parentController.position - transform.position;
+
+        // Divide by fixed timestep (default is 0.2f)
+        // so that we will get where we want to go in one step
+        forceToParentPosition = forceToParentPosition / Time.fixedDeltaTime;
+
+        // Subtract existing velocity
+        forceToParentPosition = forceToParentPosition - rigidbody.velocity;
+
+        // Add force as a VelocityChange, so it ignores mass, and effectively just sets our velocity
+        rigidbody.AddForce(forceToParentPosition, ForceMode.VelocityChange);
+    }
+
+    private void DoRotation() {
+        //Vector3 toParentRot = Quaternion.RotateTowards(transform.rotation, parentController.rotation, 0f).eulerAngles;
+
+        //rigidbody.angularVelocity = toParentRot / Time.fixedDeltaTime;
+
+        //Vector3 torqueToParentRotation = (Quaternion.Inverse(transform.rotation) * parentController.rotation).eulerAngles;
+        //Vector3 torqueToParentRotation = Quaternion.RotateTowards(transform.rotation, parentController.rotation, 0f).eulerAngles;
+        //Vector3 torqueToParentRotation = Quaternion.FromToRotation(transform.forward, parentController.forward).eulerAngles;
+
+        //torqueToParentRotation = torqueToParentRotation / Time.fixedDeltaTime;
+        //torqueToParentRotation = torqueToParentRotation - rigidbody.angularVelocity;
+
+        //rigidbody.AddTorque(torqueToParentRotation, ForceMode.VelocityChange);
+
+        //rigidbody.MoveRotation(Quaternion.FromToRotation(transform.position, parentController.position));
+
+
+
+        //Vector3 x = Vector3.Cross(transform.forward.normalized, parentController.forward.normalized);
+        //float theta = Mathf.Asin(x.magnitude);
+        //Vector3 w = x.normalized * theta / Time.fixedDeltaTime;
+
+        //Quaternion q = transform.rotation * rigidbody.inertiaTensorRotation;
+        //Vector3 T = q * Vector3.Scale(rigidbody.inertiaTensor, (Quaternion.Inverse(q) * w));
+
+        //rigidbody.AddTorque(T - rigidbody.angularVelocity, ForceMode.VelocityChange);
+
+
+        //var x = Vector3.Cross(transform.forward.normalized, parentController.forward.normalized);
+        //float theta = Mathf.Asin(x.magnitude);
+        //var w = x.normalized * theta / Time.fixedDeltaTime;
+        //var q = transform.rotation * rigidbody.inertiaTensorRotation;
+        //var t = q * Vector3.Scale(rigidbody.inertiaTensor, Quaternion.Inverse(q) * w);
+        //rigidbody.AddTorque(t - rigidbody.angularVelocity, ForceMode.Impulse);
+
+
+
+        rigidbody.rotation = parentController.rotation;
     }
 }
