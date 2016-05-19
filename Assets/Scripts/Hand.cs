0e8e6366bd6bd8242d8ecd70321f3e0445d4cbfe
David Valachovic <AdenFlorian@gmail.com>
2016-05-18 23:06:13 -0500

    cleanup

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -4,7 +4,7 @@ using System.Collections;
 [RequireComponent(typeof(Rigidbody))]
 public class Hand : MonoBehaviour {
 
-    Transform parentController;
+    Transform targetController;
     new Rigidbody rigidbody;
     
     void Start () {
@@ -17,12 +17,12 @@ public class Hand : MonoBehaviour {
     }
 
     public void SetTarget(Transform target) {
-        parentController = target;
+        targetController = target;
     }
 
     private void DoPosition() {
         // Get vector from current position to parent's position
-        Vector3 forceToParentPosition = parentController.position - transform.position;
+        Vector3 forceToParentPosition = targetController.position - transform.position;
 
         // Divide by fixed timestep (default is 0.2f)
         // so that we will get where we want to go in one step
@@ -36,42 +36,6 @@ public class Hand : MonoBehaviour {
     }
 
     private void DoRotation() {
-        //Vector3 toParentRot = Quaternion.RotateTowards(transform.rotation, parentController.rotation, 0f).eulerAngles;
-
-        //rigidbody.angularVelocity = toParentRot / Time.fixedDeltaTime;
-
-        //Vector3 torqueToParentRotation = (Quaternion.Inverse(transform.rotation) * parentController.rotation).eulerAngles;
-        //Vector3 torqueToParentRotation = Quaternion.RotateTowards(transform.rotation, parentController.rotation, 0f).eulerAngles;
-        //Vector3 torqueToParentRotation = Quaternion.FromToRotation(transform.forward, parentController.forward).eulerAngles;
-
-        //torqueToParentRotation = torqueToParentRotation / Time.fixedDeltaTime;
-        //torqueToParentRotation = torqueToParentRotation - rigidbody.angularVelocity;
-
-        //rigidbody.AddTorque(torqueToParentRotation, ForceMode.VelocityChange);
-
-        //rigidbody.MoveRotation(Quaternion.FromToRotation(transform.position, parentController.position));
-
-
-
-        //Vector3 x = Vector3.Cross(transform.forward.normalized, parentController.forward.normalized);
-        //float theta = Mathf.Asin(x.magnitude);
-        //Vector3 w = x.normalized * theta / Time.fixedDeltaTime;
-
-        //Quaternion q = transform.rotation * rigidbody.inertiaTensorRotation;
-        //Vector3 T = q * Vector3.Scale(rigidbody.inertiaTensor, (Quaternion.Inverse(q) * w));
-
-        //rigidbody.AddTorque(T - rigidbody.angularVelocity, ForceMode.VelocityChange);
-
-
-        //var x = Vector3.Cross(transform.forward.normalized, parentController.forward.normalized);
-        //float theta = Mathf.Asin(x.magnitude);
-        //var w = x.normalized * theta / Time.fixedDeltaTime;
-        //var q = transform.rotation * rigidbody.inertiaTensorRotation;
-        //var t = q * Vector3.Scale(rigidbody.inertiaTensor, Quaternion.Inverse(q) * w);
-        //rigidbody.AddTorque(t - rigidbody.angularVelocity, ForceMode.Impulse);
-
-
-
-        rigidbody.rotation = parentController.rotation;
+        rigidbody.rotation = targetController.rotation;
     }
 }
--- Assets/Scripts/SetupHands.cs
+++ Assets/Scripts/SetupHands.cs
@@ -6,21 +6,16 @@ public class SetupHands : MonoBehaviour {
     public GameObject controllerLeftGO;
     public GameObject controllerRightGO;
     public GameObject handPrefab;
-
-    // Use this for initialization
+    
     void Start () {
-        // Get reference to controllers
         GameObject handLeftGO = Instantiate<GameObject>(handPrefab);
         GameObject handRightGO = Instantiate<GameObject>(handPrefab);
-        //handLeftGO.transform.parent = controllerLeftGO.transform;
-        //handRightGO.transform.parent = controllerRightGO.transform;
         handLeftGO.transform.position = controllerLeftGO.transform.position;
         handRightGO.transform.position = controllerRightGO.transform.position;
         handLeftGO.GetComponent<Hand>().SetTarget(controllerLeftGO.transform);
         handRightGO.GetComponent<Hand>().SetTarget(controllerRightGO.transform);
     }
-
-    // Update is called once per frame
+    
     void Update () {
 	
 	}
